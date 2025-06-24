99e3452f417b0ef8cc310ab5f518810917b3777a
Victor Basumatary <vlenceleuth@gmail.com>
2025-06-24 23:29:29 +0530

    Implement Ok()

---
+++ gossert.go
@@ -0,0 +1,25 @@
+/*
+Package gossert exports functions to perform assertions. Assertions are checks
+that your program must pass. If an assertion fails then the current goroutine
+will panic.
+
+Usage:
+
+        package main
+
+        import "github.com/vlence/gossert"
+
+        func main() {
+                gossert.Ok(true, 'you will never see this error message') 
+        }
+*/
+package gossert
+
+import "errors"
+
+// Ok panics if the given condition is not true
+func Ok(cond bool, msg string) {
+        if !cond {
+                panic(errors.New(msg))
+        }
+}
