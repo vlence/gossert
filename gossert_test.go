452775050d99aa6bf194c5e2edb5ab80a8cba414
Victor Basumatary <vlenceleuth@gmail.com>
2026-04-22 21:25:26 +0800

    Add tests for NilErr

--- gossert_test.go
+++ gossert_test.go
@@ -1,6 +1,9 @@
 package gossert
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestOkDoesNotPanicWhenGivenTrue(t *testing.T) {
         defer func () {
@@ -37,4 +40,53 @@ func TestOkPanicsWhenGivenFalse(t *testing.T) {
         Ok(false, msg)
 
         t.Errorf("Ok did not panic")
+}
+
+func TestNilErrPanicsWhenErrIsNotNil(t *testing.T) {
+	cause := errors.New("assertion error cause")
+	msg := "assertion failed"
+
+	defer func(msg string, cause error) {
+		r := recover()
+
+		if r == nil {
+			t.Fatalf("expected assertion error but got nil")
+		}
+
+		err, ok := r.(*Error)
+
+		if !ok {
+			t.Fatalf("expected to recover assertion error but got %#v", r)
+		}
+
+		if err == nil {
+			t.Fatalf("expected assertion error but got nil")
+		}
+
+		if err.cause != cause {}
+
+		if err.msg != msg {}
+	}(msg, cause)
+
+	NilErr(cause, msg)
+
+	t.Errorf("NilError did not panic")
+}
+
+func TestNilErrDoesNotPanicWhenErrIsNil(t *testing.T) {
+	defer func() {
+		r := recover()
+
+		if r != nil {
+			t.Logf("expected nil but got %#v", r)
+		}
+
+		err, ok := r.(error)
+
+		if ok {
+			t.Fatalf("unexpected error %s", err.Error())
+		}
+	}()
+
+	NilErr(nil, "I should not see this")
 }
